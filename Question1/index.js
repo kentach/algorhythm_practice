db337f23697c9f51f3dbc686d671d6f636b206dc
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-07-28 14:32:29 +0900

    Question1/index.js

--- Question1/index.js
+++ Question1/index.js
@@ -27,5 +27,20 @@
   console.log(solution("123456789"))
   console.log(solution("abcdefghijklmnopqrstuvwxyz"))
 
+
+  // filterを使ったやり方
+  // 1. 引数をスプレッド構文を使って配列に変換する
+  // 2. filterメソッドを使用し、return以降に条件を記述。
+  // 3. その返り値をarrayで受け取り、joinメソッドで配列から文字列に変換
+
+  function filterMethod (A) {
+    const array = [...A].filter((_, index) => {
+      const stringIndex = index + 1
+      return index % 3 === 0 || String(index).includes('3')
+    })
+    return array.join("")
+  }
+
+  console.log(solution("abcdefghijklmnopqrstuvwxyz"));
 }
   
