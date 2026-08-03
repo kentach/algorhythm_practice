83612c49a237743cbc3a56c7b757fb50a4cc7a2d
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-08-03 10:06:05 +0900

    update/Question4_to_9

--- Question5/index.js
+++ Question5/index.js
@@ -1,17 +1,16 @@
 {
-  function findSameWord(S) {
+  function findSameChar(S) {
     let count = 0;
-
-    for (let i = 0; i < S.length - 1; i++) {
+    for (let i = 0; i < S.length; i++) {
       if (S[i] === S[i + 1]) {
-        count++
+        count++;
       }
     }
-    return count
+    return count;
   }
-  console.log(findSameWord("aabbcc"));
-  console.log(findSameWord("abca"));
-  console.log(findSameWord("abcccde"));
-  
-  
+  console.log(findSameChar("aabbcc"));
+  console.log(findSameChar("abca"));
+  console.log(findSameChar("aaa"));
+  console.log(findSameChar("abcccde"));
+  console.log(findSameChar("a"));
 }
--- Question7/index.js
+++ Question7/index.js
@@ -1,15 +1,22 @@
 {
   function findDifferentWords(S) {
-    let result = "";
-
-    for (const n of S) {
-      if (!result.includes(n)) {
-        result += n
+    let result = ""
+    for (const char of S) {
+    if (!result.includes(char)) {
+        result += char
       }
     }
     return result.length
+    
   }
-  console.log(findDifferentWords("banana")); 
+  console.log(findDifferentWords("banana"));
   console.log(findDifferentWords("aabbcc"));
+
+  // セット関数を使った方法
+  function set(S){
+    return new Set(S).size
+  }
+  console.log(set("banana"));
+  
   
 }
